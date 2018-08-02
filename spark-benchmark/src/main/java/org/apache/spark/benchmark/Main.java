576e5235530f3fbe82e9408e502e2f73a547b429
Yury Babak <ybabak@gridgain.com>
2018-08-02 17:48:54 +0300

    Random Forest for Spark 5

--- spark-benchmark/src/main/java/org/apache/spark/benchmark/Main.java
+++ spark-benchmark/src/main/java/org/apache/spark/benchmark/Main.java
@@ -17,12 +17,19 @@
 
 package org.apache.spark.benchmark;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.classification.ClassificationModel;
 import org.apache.spark.ml.classification.DecisionTreeClassificationModel;
 import org.apache.spark.ml.classification.DecisionTreeClassifier;
+import org.apache.spark.ml.classification.GBTClassificationModel;
+import org.apache.spark.ml.classification.GBTClassifier;
+import org.apache.spark.ml.classification.LinearSVC;
+import org.apache.spark.ml.classification.LinearSVCModel;
 import org.apache.spark.ml.classification.RandomForestClassificationModel;
 import org.apache.spark.ml.classification.RandomForestClassifier;
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
@@ -35,16 +42,21 @@ import org.apache.spark.sql.SparkSession;
  * TODO: add description.
  */
 public class Main {
+    /** Path to dataset. */
+    private static String datapath = "/home/ybabak/Downloads/homecredit_with_column_numbers.csv";
 
+    private static Map<String, ResultEntry> results;
+
+    /** */
     public static void main(String[] argsList){
-        SparkConf sparkConf = new SparkConf().setAppName("JavaRandomForestClassificationExample").setMaster("local");
-
+        SparkConf sparkConf = new SparkConf().setAppName("ClassificationBenchmark").setMaster("local");
         JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-// Load and parse the data file.
-        String datapath = "/home/ybabak/Downloads/homecredit_with_column_numbers.csv";
-        SparkSession sparkSession = SparkSession.builder().appName("JavaRandomForestClassificationExample").getOrCreate();
+        SparkSession sparkSes = SparkSession.builder().appName("ClassificationBenchmark").getOrCreate();
 
-        final Dataset<Row> ds = sparkSession.read().option("header", true).option("inferSchema", "true").csv(datapath);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> START DATA LOADING");
+        long startTime = System.currentTimeMillis();
+
+        final Dataset<Row> ds = sparkSes.read().option("header", true).option("inferSchema", "true").csv(datapath);
         Dataset<Row>[] datasets = ds.randomSplit(new double[] {0.6, 0.4});
 
         Dataset<Row> trainDataset = datasets[0];
@@ -62,10 +74,75 @@ public class Main {
         Dataset<Row> trainSet = assembler.transform(trainDataset);
         Dataset<Row> testSet = assembler.transform(testDataset);
 
+        long timeDelta = System.currentTimeMillis() - startTime;
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> FINISHED DATA LOADING");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> TIME FOR LOADING: " + format(timeDelta));
+
+        results = new HashMap<>();
+
 //        rfBenchmark(trainSet, testSet);
-        dtBenchmark(trainSet, testSet);
+//        dtBenchmark(trainSet, testSet);
+        gdbBenchmark(trainSet, testSet);
+        svmBenchmark(trainSet, testSet);
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> RESULTS:");
+
+        results.forEach((k,v) -> {
+            System.out.println(k);
+            System.out.println("time: " + format(v.millis));
+            System.out.println("accuracy " + v.accuracy);
+        });
     }
 
+    /**
+     * SVM benchmark.
+     *
+     * @param trainSet Train set.
+     * @param testSet Test set.
+     */
+    private static void svmBenchmark(Dataset<Row> trainSet, Dataset<Row> testSet) {
+        System.out.println(String.format(">>>>>>>>>>>>>>>>>>>>>> START BENCHMARK [%s mode]", "SVM"));
+        long startTime = System.currentTimeMillis();
+
+        LinearSVC trainer = new LinearSVC()
+            .setLabelCol("TARGET");
+
+        LinearSVCModel mdl = trainer.fit(trainSet);
+
+        long timeDelta = System.currentTimeMillis() - startTime;
+
+        evaluateModel(mdl, timeDelta, testSet);
+    }
+
+    /**
+     * Gradient boosting on trees benchmark.
+     *
+     * @param trainSet Train set.
+     * @param testSet Test set.
+     */
+    private static void gdbBenchmark(Dataset<Row> trainSet, Dataset<Row> testSet) {
+        System.out.println(String.format(">>>>>>>>>>>>>>>>>>>>>> START BENCHMARK [%s mode]", "GDB"));
+        long startTime = System.currentTimeMillis();
+
+        GBTClassifier trainer = new GBTClassifier()
+            .setLabelCol("TARGET")
+            .setMaxDepth(1)
+            .setStepSize(1.0d)
+            .setMaxIter(500);
+
+        GBTClassificationModel mdl = trainer.fit(trainSet);
+
+        long timeDelta = System.currentTimeMillis() - startTime;
+
+        evaluateModel(mdl, timeDelta, testSet);
+    }
+
+    /**
+     * DecisionTree benchmark.
+     *
+     * @param trainSet Train set.
+     * @param testSet Test set.
+     */
     private static void dtBenchmark(Dataset<Row> trainSet, Dataset<Row> testSet) {
         System.out.println(String.format(">>>>>>>>>>>>>>>>>>>>>> START BENCHMARK [%s mode]", "DecisionTree"));
         long startTime = System.currentTimeMillis();
@@ -75,23 +152,19 @@ public class Main {
             .setMaxDepth(10)
             .setMinInfoGain(0.0d);
 
-        DecisionTreeClassificationModel model = trainer.fit(trainSet);
+        DecisionTreeClassificationModel mdl = trainer.fit(trainSet);
 
         long timeDelta = System.currentTimeMillis() - startTime;
 
-        Dataset<Row> output = model.transform(testSet);
-
-        MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
-            .setLabelCol("TARGET")
-            .setPredictionCol("prediction")
-            .setMetricName("accuracy");
-
-        double accuracy = evaluator.evaluate(output);
-
-        System.out.println("time: " + format(timeDelta));
-        System.out.println("accuracy " + accuracy);
+        evaluateModel(mdl, timeDelta, testSet);
     }
 
+    /**
+     * RandomForest benchmark.
+     *
+     * @param trainSet Train set.
+     * @param testSet Test set.
+     */
     private static void rfBenchmark(Dataset<Row> trainSet, Dataset<Row> testSet) {
         System.out.println(String.format(">>>>>>>>>>>>>>>>>>>>>> START BENCHMARK [%s mode]", "RandomForest"));
         long startTime = System.currentTimeMillis();
@@ -103,9 +176,22 @@ public class Main {
             .setMinInfoGain(0.0d)
             .setSubsamplingRate(0.1)
             .setFeatureSubsetStrategy("sqrt");
-        RandomForestClassificationModel model = trainer.fit(trainSet);
+        RandomForestClassificationModel mdl = trainer.fit(trainSet);
 
-        Dataset<Row> output = model.transform(testSet);
+        long timeDelta = System.currentTimeMillis() - startTime;
+
+        evaluateModel(mdl, timeDelta, testSet);
+    }
+
+    /**
+     * Evaluate model and print result, time and accuracy.
+     *
+     * @param mdl Model.
+     * @param millis Millis.
+     * @param testSet Test set.
+     */
+    private static void evaluateModel(ClassificationModel mdl, long millis, Dataset<Row> testSet){
+        Dataset<Row> output = mdl.transform(testSet);
 
         MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
             .setLabelCol("TARGET")
@@ -114,12 +200,17 @@ public class Main {
 
         double accuracy = evaluator.evaluate(output);
 
-        long timeDelta = System.currentTimeMillis() - startTime;
-
-        System.out.println("time: " + format(timeDelta));
+        System.out.println("time: " + format(millis));
         System.out.println("accuracy " + accuracy);
+
+        results.put(mdl.getClass().getSimpleName(), new ResultEntry(millis, accuracy));
     }
 
+    /**
+     * Format time form long to readable format.
+     *
+     * @param millis Millis.
+     */
     private static String format(long millis){
         return String.format("%02d:%02d:%02d",
             TimeUnit.MILLISECONDS.toHours(millis),
@@ -127,5 +218,15 @@ public class Main {
                 TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), // The change is in this line
             TimeUnit.MILLISECONDS.toSeconds(millis) -
                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+    }
+
+    private static class ResultEntry{
+        private long millis;
+        private double accuracy;
+
+        public ResultEntry(long millis, double accuracy) {
+            this.millis = millis;
+            this.accuracy = accuracy;
+        }
     }
 }
